4e58fdefac813fbce53021717b2a8c45825cd6eb
aedh carrick <aedhcarrick@gmail.com>
2024-03-24 20:42:55 +0000

    breaktime.

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,13 +4,16 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
-
+    df = pd.read_csv('epa-sea-level.csv', parse_dates=True)
     # Create scatter plot
-
-
+    x = df['Year']
+    y = df['CSIRO Adjusted Sea Level']
+    plt.scatter(x, y)
     # Create first line of best fit
-
+    result = linregress(x.to_numpy(), y.to_numpy())
+    slope = result.slope
+    intercept = result.intercept
+    plt.plot(x, intercept + slope * x, '-')
 
     # Create second line of best fit
 
